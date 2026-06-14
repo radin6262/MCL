4258bb4c9f64dea15fb358bdea8ed3b3c2e71d01
radin6262 <radindavari054@gmail.com>
2026-06-14 23:54:07 +0330

    changed nutika build to use UPX compression

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-nuitka --script=main.py --standalone --onefile --enable-plugin=pyside6 --windows-console-mode=disable --include-module=base,offline,launcher --include-package=requests --nofollow-import-to=cryptography --output-dir=dist --assume-yes-for-downloads
+nuitka --script=main.py --standalone --onefile --onefile-no-compression --enable-plugin=pyside6 --windows-console-mode=disable --include-module=base,offline,launcher --include-package=requests --nofollow-import-to=cryptography --output-dir=dist --assume-yes-for-downloads --enable-plugin=upx --upx-binary="D:\upx\upx.exe"
